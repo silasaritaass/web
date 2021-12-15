c4f0c3f1c76fa379f9669c06f1b374a488c730de
silasaritaass <91497175+silasaritaass@users.noreply.github.com>
2021-12-15 14:26:07 +0300

    Add files via upload

---
+++ 14.12.2021/sss.php
@@ -0,0 +1,56 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Başlıksız Belge</title>
+<style type="text/css">
+p {
+	font-family: "Times New Roman", Times, serif;
+	font-size: 20pt;
+	color: #F00;
+	background-color: #3F3;
+}
+.kutu1 {
+}
+.kutu1 {
+	font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+	font-size: 16pt;
+	color: #F00;
+	background-color: #FF3;
+	text-align: center;
+	height: 150px;
+	width: 200px;
+	border: 2px solid #066;
+	position: absolute;
+	left: 362px;
+	top: 171px;
+}
+.kutu2 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 16px;
+	font-style: italic;
+	font-weight: bold;
+	color: #F00;
+	text-decoration: underline;
+	background-color: #09F;
+	background-image: url(3.jpg);
+	background-repeat: repeat-y;
+	background-position: right;
+	border: 3px inset #0F9;
+	position: absolute;
+	width: 209px;
+	height: 258px;
+	left: 61px;
+	top: 139px;
+}
+</style></head>
+
+<body>
+<div>
+<p>BİLİŞİM tEKNOLOJİLERİ</p>
+</div>
+<p>2022 GELİYO PİYANGO bileti AL ABİ:)</p>
+<div class="kutu1">sdasdasd</div>
+<div class="kutu2">ssadsadsdsdsdasds</div>
+</body>
+</html>
